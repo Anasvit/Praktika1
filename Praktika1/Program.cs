29d8eb201ec1dff3ebe2f39239d520c4fea83737
GuselnikovaAV <GuselnikovaAV@310D-9>
2024-12-05 09:26:22 +0700

    Добавьте файлы проекта.

---
+++ Praktika1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(CountBits(4));   
+        Console.WriteLine(CountBits(10));  
+        Console.WriteLine(CountBits(1));  
+        Console.WriteLine(CountBits(32));  
+    }
+
+    static int CountBits(int num)
+    {
+        string binary = Convert.ToString(num, 2);
+
+        int count = 0;
+        
+        foreach (char bit in binary)
+        {
+            if (bit == '1')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
